8de2b1835963c799563eaceb307042a781dae8a8
Luca Soldaini <lucas@allenai.org>
2022-08-15 13:32:25 -0700

    fixed small bug

--- iterm2-scripts/AutoLaunch/auto_dark_mode.py
+++ iterm2-scripts/AutoLaunch/auto_dark_mode.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 
-import asyncio
-import iterm2
+import iterm2   # type: ignore
 from dataclasses import dataclass
-from typing import Type, TypeVar
+from typing import Generic, Type, TypeVar, Union
 
 
 FS = TypeVar('FS', bound='FontSpec')
 
+
 @dataclass
-class FontSpec:
+class FontSpec(Generic[FS]):
     name: str
     size: int
-    weight: str = None
+    weight: Union[str, None] = None
 
     @classmethod
     def from_string(cls: Type[FS], string: str) -> FS:
         name_weight, size, *_ = string.split(' ')
         name, *weight = name_weight.split('-')
-        weight = weight or None
-        return FontSpec(name=name, weight=weight[0], size=int(size))
+        return cls(
+            name=name,
+            weight=(weight[0] if len(weight) else None),
+            size=int(size)
+        )
 
     def to_string(self: FS) -> str:
         weight = f'-{self.weight}' if self.weight else ''
@@ -27,23 +30,36 @@ class FontSpec:
 
 
 async def main(connection):
-    monitor_args = (connection, iterm2.VariableScopes.APP, "effectiveTheme", None)
-    async with iterm2.VariableMonitor(*monitor_args) as mon:
+    monitor_args = (
+        connection,
+        iterm2.VariableScopes.APP,      # type: ignore
+        "effectiveTheme",
+        None
+    )
+    async with iterm2.VariableMonitor(*monitor_args) as mon:    # type: ignore
         while True:
             # Block until theme changes
             theme = await mon.async_get()
 
-            # Themes have space-delimited attributes, one of which will be light or dark.
+            # Themes have space-delimited attributes, one of
+            # which will be light or dark.
             parts = theme.split(" ")
             if "dark" in parts:
-                preset = await iterm2.ColorPreset.async_get(connection, "Dracula")
+                preset = await iterm2.ColorPreset.async_get(   # type: ignore
+                    connection, "Dracula"
+                )
                 font = FontSpec('FiraCodeRoman', 15, 'Regular')
             else:
-                preset = await iterm2.ColorPreset.async_get(connection, "one-light-terminal")
+                preset = await iterm2.ColorPreset.async_get(    # type: ignore
+                    connection,
+                    "one-light-terminal"
+                )
                 font = FontSpec('FiraCodeRoman', 15, 'Medium')
 
             # Update the list of all profiles and iterate over them.
-            profiles = await iterm2.PartialProfile.async_query(connection)
+            profiles = await iterm2.PartialProfile.async_query(  # type: ignore
+                connection
+            )
             for partial in profiles:
                 # Fetch the full profile and then set the color preset in it.
                 profile = await partial.async_get_full_profile()
@@ -54,4 +70,4 @@ async def main(connection):
                 # await change.async_set_color_preset(preset)
                 await profile.async_set_color_preset(preset)
 
-iterm2.run_forever(main)
+iterm2.run_forever(main)    # type: ignore
