4f5dad72043bc52e226c06f399be82989548dade
Luca Soldaini <lucas@allenai.org>
2025-01-27 08:04:28 -0800

    windows

--- macos_setup.sh
+++ macos_setup.sh
@@ -19,35 +19,24 @@ defaults write -globalDomain NSUserKeyEquivalents -dict-add "Merge All Windows" 
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Show Help Menu" "~ "
 defaults write -globalDomain NSUserKeyEquivalents -dict-add "Edit Tab Title" "@~^e"
 
-# # Change tiling shortcut to work on external keyboard w/o fn key
-# ## Fullscreen is ⌃⌥↩
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Fill" "~^\\U21a9"
-#
-# ## Return to Previous Size is ⌃⌥R
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Return to Previous Size" "~^r"
-#
-# ## Arranging to half size for left (⌃⌥←), right (⌃⌥→), top (⌃⌥↑), bottom (⌃⌥↓)
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Left" "~^\\U2190"
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Right" "~^\\U2192"
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Top" "~^\\U2191"
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Bottom" "~^\\U2193"
-#
-# ## Arranging to quarters of the screen with top left (⌃⌥H), top right (⌃⌥L), bottom left (⌃⌥J), bottom right (⌃⌥K)
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Top Left" "~^h"
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Top Right" "~^l"
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Bottom Left" "~^j"
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Bottom Right" "~^k"
-#
-# ## Tiles two windows side by side (⌃⌥←) and (⌃⌥→)
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Left & Right" "@~^\\U2190"
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Right & Left" "@~^\\U2192"
-#
-# ## Tiles two windows on top of each other (⌃⌥↑) and (⌃⌥↓)
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Top & Bottom" "@~^\\U2191"
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Bottom & Top" "@~^\\U2193"
-#
-# ## Tiles four windows in a 2x2 grid (⌃⌥/)
-# defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Quarters" "@~^/"
+# Change tiling shortcut to work on external keyboard w/o fn key
+## Fullscreen is ⌃⌥↩
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Fill" "~^\\U21a9"
+
+## Return to Previous Size is ⌃⌥R
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Return to Previous Size" "~^r"
+
+## Arranging to half size for left (⌃⌥←), right (⌃⌥→), top (⌃⌥↑), bottom (⌃⌥↓)
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Left" "~^\\U2190"
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Right" "~^\\U2192"
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Top" "~^\\U2191"
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Bottom" "~^\\U2193"
+
+## Arranging to quarters of the screen with top left (⌃⌥H), top right (⌃⌥L), bottom left (⌃⌥J), bottom right (⌃⌥K)
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Top Left" "~^h"
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Top Right" "~^l"
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Bottom Left" "~^j"
+defaults write -globalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Bottom Right" "~^k"
 
 # Make ⌘Q keep window, ⌘⌥Q not
 defaults write com.apple.Safari NSUserKeyEquivalents -dict-add "Quit Safari" "@~q"
